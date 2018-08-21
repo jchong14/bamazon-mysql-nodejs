d5e907f13c06bdfd7aeca1e4a2a6abb564e270c1
Ji Chong <ji_no8@yahoo.com>
2018-08-20 20:26:25 -0700

    finishing touches

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -36,52 +36,62 @@ function loadProducts(){
 
 //prompt the customer for product ID
 
-function promptCustomerForItem (inventory) {
+function promptCustomerForItem(inventory) {
+  // Prompts user for what they would like to purchase
   inquirer
     .prompt([
       {
         type: "input",
         name: "choice",
-        message: "what is the I.D. of the item you would like to purchase?[quit with q]",
-        validate: function(val){
-          return ! isNaN || val.toLowerCase() === "q";
+        message: "What is the ID of the item you would you like to purchase? [Quit with Q]",
+        validate: function(val) {
+          return !isNaN(val) || val.toLowerCase() === "q";
         }
       }
-    ]) .then(function (val){
+    ])
+    .then(function(val) {
+      // Check if the user wants to quit the program
       checkIfShouldExit(val.choice);
       var choiceId = parseInt(val.choice);
       var product = checkInventory(choiceId, inventory);
 
-
+      // If there is a product with the id the user chose, prompt the customer for a desired quantity
       if (product) {
-        promptCustomerForQuantity();
-      }else {
-        console.log("That item doesn't exist");
+        // Pass the chosen product to promptCustomerForQuantity
+        promptCustomerForQuantity(product);
+      }
+      else {
+        // Otherwise let them know the item is not in the inventory, re-run loadProducts
+        console.log("\nThat item is not in the inventory.");
         loadProducts();
       }
     });
 }
 
-function promptCustomerForQuantity(product){
+function promptCustomerForQuantity(product) {
   inquirer
     .prompt([
       {
         type: "input",
         name: "quantity",
-        message: "How many would your like?[quit with q]",
-        validate: function(val){
+        message: "How many would you like? [Quit with Q]",
+        validate: function(val) {
           return val > 0 || val.toLowerCase() === "q";
         }
       }
-    ]) .then(function (val){
+    ])
+    .then(function(val) {
+      // Check if the user wants to quit the program
       checkIfShouldExit(val.quantity);
       var quantity = parseInt(val.quantity);
 
-
-
+      // If there isn't enough of the chosen product and quantity, let the user know and re-run loadProducts
       if (quantity > product.stock_quantity) {
-        console.log("insufficient quantities");
-      }else {
+        console.log("\nInsufficient quantity!");
+        loadProducts();
+      }
+      else {
+        // Otherwise run makePurchase, give it the product information and desired quantity to purchase
         makePurchase(product, quantity);
       }
     });
